d70c516e30884b4c347b0dcfb2e9f820557c4a46
SungbinYang <ysb05222@naver.com>
2021-04-11 23:37:01 +0900

    Initial commit

---
+++ src/test/java/me/sungbin/uwahanadminserver/UwahanAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sungbin.uwahanadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwahanAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
